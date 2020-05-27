789b24076a9b38d7287a79e82ff785f4893bd223
Jose Ferreira <jferreira590@gmail.com>
2020-05-27 14:59:12 -0400

    Adding project images and content to portfolio.

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,15 @@
+import dsbotHome from "./images/dsbot_home.png"
+import mlbh2hSearch from "./images/mlbh2h_search.png"
+import redsquirrelHome from "./images/redsquirrel_home.png"
+import cathay22 from "./images/cathay22_hero.png"
+import outrspaceHome from "./images/outrspace_home.png"
+
 const projects = [
   {
     name: "DSbot",
     description:
       "Built with HTML, CSS, and vanilla JavaScript, DSbot is your personal drill sergeant! ",
-    image: "",
+    image: dsbotHome,
     githubURL: "https://github.com/joseferreira5/DSbot",
     deployedURL: "http://dsbot.surge.sh/",
   },
@@ -11,7 +17,7 @@ const projects = [
     name: "MLB H2H",
     description:
       "MLB Head to Head Player Comparison app built with React (Hooks).",
-    image: "",
+    image: mlbh2hSearch,
     githubURL: "https://github.com/joseferreira5/mlb-h2h",
     deployedURL: "https://mlb-h2h.netlify.app/",
   },
@@ -19,7 +25,7 @@ const projects = [
     name: "Red Squirrel",
     description:
       "An inventory management web application built with the MERN stack.",
-    image: "",
+    image: redsquirrelHome,
     githubURL: "https://github.com/joseferreira5/red-squirrel",
     deployedURL: "http://www.red-squirrel.surge.sh/",
   },
@@ -27,7 +33,7 @@ const projects = [
     name: "Cathay22",
     description:
       "Cathay 22 was designed by a team of UX designers for a family owned restaurant in New Jersey. Built with React, in collaboration with a team of three software engineers.",
-    image: "",
+    image: cathay22,
     githubURL: "https://github.com/joseferreira5/cathay-22",
     deployedURL: "https://cathay-22.netlify.app/",
   },
@@ -35,7 +41,7 @@ const projects = [
     name: "Outr Space",
     description:
       "Outr Space is a message board and a full-stack web application built with React and Ruby on Rails.",
-    image: "",
+    image: outrspaceHome,
     githubURL: "https://github.com/joseferreira5/outr-space",
     deployedURL: "https://outrspace.netlify.app/",
   },
